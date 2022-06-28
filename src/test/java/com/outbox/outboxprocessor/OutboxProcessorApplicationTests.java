0ce24b7a7f4ad97d16f3be753da2325ddd8d4f8c
monjure-alam <monjure.alam@poinsort.com>
2022-06-28 20:53:46 +0530

    initial commit

---
+++ src/test/java/com/outbox/outboxprocessor/OutboxProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outbox.outboxprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutboxProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
